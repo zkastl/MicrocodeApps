b6858d337776e1b8b9720afa6ddfbc98fecb5a24
Zachary Kastl <zkastl8@gmail.com>
2019-11-08 17:20:23 -0600

    added some boot experiments

--- boot.asm
+++ boot.asm
@@ -1,4 +1,15 @@
 org 0x7c00
+
+cls:
+    mov bx, 0x000f
+    mov ax, 0x0e0a
+    mov cx, 0x25
+cls_loop:
+    int 0x10
+    cmp cx, 0x00
+    je start
+    dec cx
+    jmp cls_loop
 start:
     push cs                 ; Assumes CS contains 0x0000
     pop ds
@@ -16,6 +27,7 @@ repeat:
     jmp repeat
 end:
     jmp $                   ; Jump over itself ($ gives current address)
+
 string:
     db "Hello, World!",0
     times 510-($-$$) db 0   ; Fills boot sector (510 bytes)
