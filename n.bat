78f134eb3c8d12fa25b8eaa507268a8d40c079c0
Zachary Kastl <zkastl8@gmail.com>
2019-11-07 21:35:39 -0600

    added boot sector apps; updated script

--- n.bat
+++ n.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%1"=="" goto error
-nasm -fbin %1.asm -l %1.lst -o %1.com
-dosbox -c cls %1.com
+nasm -fbin %1.asm -l %1.lst -o %1.img
+qemu-system-x86_64 -fda %1.img
 goto end
 :error
 echo What do you want to Make?
